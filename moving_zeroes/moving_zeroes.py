4c190c3804ab13388703587e2a53630be881e3f6
Chad Parker <chad@chadparker.com>
2020-10-06 20:27:14 -0700

    Implement moving_zeroes()

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,19 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # leftNumbers = []
+    # rightNumbers = []
+    # for num in arr:
+    #     if num == 0:
+    #         rightNumbers.append(num)
+    #     else:
+    #         leftNumbers.append(num)
+    # return leftNumbers + rightNumbers
 
-    pass
-
+    # alternate using list comprehensions:
+    leftNumbers = [num for num in arr if num != 0]
+    rightNumbers = [num for num in arr if num == 0]
+    return leftNumbers + rightNumbers
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
